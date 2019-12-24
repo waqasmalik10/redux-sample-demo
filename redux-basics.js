b97e89eb0cb2ed0dc0b7dca3a51e17fcb6f34774
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-24 12:11:05 +0500

    updated comments

--- redux-basics.js
+++ redux-basics.js
@@ -5,6 +5,7 @@ const initialState = {
     counter: 0
 }
 
+
 // Reducer
 const rootReducer = (state = initialState, action) => {
     if(action.type==='INC_COUNTER') {
@@ -22,21 +23,19 @@ const rootReducer = (state = initialState, action) => {
     return state;
 }
 
+
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+
+// Subscription 
 store.subscribe(() => {
     console.log('[Subscribe]' , store.getState());
 })
 
+
+// Dispacthing Action 
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-
-
-
-// Dispacthing Action 
-
-
-// Subscription 
